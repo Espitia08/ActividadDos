b20b4f347160002cc2fb9263361d597ecf6c4abf
Espitia08 <brayan.cordoba@uniminuto.edu.co>
2025-04-16 23:39:00 -0500

    Update MainActivity.java

--- app/src/main/java/co/edu/uiminuto/segundaact/MainActivity.java
+++ app/src/main/java/co/edu/uiminuto/segundaact/MainActivity.java
@@ -1,86 +1,89 @@
-package co.edu.uiminuto.segundaact;
+package com.example.actividadtres;
 
-import static android.widget.Toast.makeText;
-
-import android.content.Context;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Set;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends Activity {
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    private EditText etTask;
-    private Button btnAdd;
-    private ListView listTask;
-    private ArrayList<String> arrayList;
-    private ArrayAdapter<String> adapter;
+    private BluetoothAdapter bluetoothAdapter;
+    private TextView statusView;
+    private ImageView imageView;
+    private EditText fileNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        initObject();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        statusView = findViewById(R.id.statusView);
+        imageView = findViewById(R.id.imageView);
+        fileNameInput = findViewById(R.id.fileNameInput);
 
-        this.btnAdd.setOnClickListener(this::addTask);
+        Button checkBluetooth = findViewById(R.id.checkBluetooth);
+        Button toggleBluetooth = findViewById(R.id.toggleBluetooth);
+        Button pairedDevices = findViewById(R.id.pairedDevices);
+        Button takePhoto = findViewById(R.id.takePhoto);
+        Button saveFile = findViewById(R.id.saveFile);
 
-        this.listTask.setOnItemClickListener((parent, view, position, id) -> {
-            String task = arrayList.get(position);
-            Intent intent = new Intent(MainActivity.this, TaskDetailActivity.class);
-            intent.putExtra("task", task);
-            intent.putExtra("position", position);
-            startActivityForResult(intent, 1);
-        });
-    }
-
-    private void addTask(View view) {
-        String task = this.etTask.getText().toString().trim();
-        if (!task.isEmpty()) {
-            this.arrayList.add(task);
-            this.adapter.notifyDataSetChanged();
-            this.etTask.setText("");
-        } else {
-            Toast.makeText(this, "Coloque una tarea", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void initObject() {
-        this.etTask = findViewById(R.id.etTask);
-        this.btnAdd = findViewById(R.id.btnAdd);
-        this.listTask = findViewById(R.id.listTask);
-        this.arrayList = new ArrayList<>();
-        this.adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, this.arrayList);
-        this.listTask.setAdapter(this.adapter);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            String updatedTask = data.getStringExtra("updatedTask");
-            int position = data.getIntExtra("position", -1);
-            boolean isDeleted = data.getBooleanExtra("isDeleted", false);
-
-            if (isDeleted && position != -1) {
-                arrayList.remove(position);
-            } else if (updatedTask != null && position != -1) {
-                arrayList.set(position, updatedTask);
+        checkBluetooth.setOnClickListener(v -> {
+            if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+                statusView.setText("Bluetooth está activado");
+            } else {
+                statusView.setText("Bluetooth está desactivado");
             }
-            adapter.notifyDataSetChanged();
-        }
+        });
+
+        toggleBluetooth.setOnClickListener(v -> {
+            if (bluetoothAdapter != null) {
+                if (!bluetoothAdapter.isEnabled()) {
+                    startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 1);
+                } else {
+                    bluetoothAdapter.disable();
+                }
+            }
+        });
+
+        pairedDevices.setOnClickListener(v -> {
+            if (bluetoothAdapter != null && bluetoothAdapter.isEnabled()) {
+                Set bondedDevices = bluetoothAdapter.getBondedDevices();
+                StringBuilder devices = new StringBuilder();
+                for (Object device : bondedDevices) {
+                    devices.append(device.toString()).append("\n");
+                }
+                statusView.setText(devices.toString());
+            }
+        });
+
+        takePhoto.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 100);
+        });
+
+        saveFile.setOnClickListener(v -> {
+            String fileName = fileNameInput.getText().toString();
+            File path = getExternalFilesDir(null);
+            File file = new File(path, fileName + ".txt");
+            try {
+                FileOutputStream stream = new FileOutputStream(file);
+                BatteryManager bm = (BatteryManager)getSystemService(BATTERY_SERVICE);
+                int level = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+                String content = "Estudiante: Tu Nombre\nBatería: " + level + "%\nVersión Android: " + android.os.Build.VERSION.RELEASE;
+                stream.write(content.getBytes());
+                stream.close();
+                statusView.setText("Archivo guardado");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
